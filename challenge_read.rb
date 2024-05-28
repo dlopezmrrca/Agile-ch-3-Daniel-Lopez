4fc904e35753765ff58a93cac671bbccf2e25c84
Daniel <dlopezmaldonado@rrc.ca>
2024-05-28 19:22:42 +0000

    Updates to read and product

--- challenge_read.rb
+++ challenge_read.rb
@@ -3,3 +3,33 @@ require_relative 'ar.rb'
 number_of_products = Product.count
 
 puts "There are #{number_of_products} in the products table."
+
+# To load-up all rows from our Product table we call .all.
+
+all_products = Product.all
+
+# If we know the primary key we can search for it directly with find.
+
+product_by_id = Product.find()
+
+puts product_by_id.inspect
+
+# Get the column information, create an object with all columns name
+columns = Product.columns.map { |c| "#{c.name}: #{c.type}" }
+
+# Print the columns after creating the object
+puts columns
+
+# Products with names starting with 'C' and price greater than $10
+products_above_10_with_c = Product.where("name LIKE ? AND price > ?", 'C%', 10)
+
+# Print the names of these products
+products_above_10_with_c.each do |product|
+  puts product.name
+end; nil
+
+# Count of products with stock quantity less than 5
+low_stock_count = Product.where("stock_quantity < ?", 5).count
+
+# Print Count of products with stock quantity less than 5
+puts "Total number of products with low stock quantity: #{low_stock_count}"
--- models/product.rb
+++ models/product.rb
@@ -1,8 +1,19 @@
 class Product < ActiveRecord::Base
   # This AR object is linked with the products table.
-  
-  # A product has a many to one relationship with a category.
+
+  # A product has a many-to-one relationship with a category.
   # The products table has a category_id foreign key.
   # In other words, a product belongs to a category.
   belongs_to :category
+
+  # Add a validation to this class such that a product must have all columns (other than foreign keys, the id, or datetime column) filled out before it can be saved to the db.
+  validates :name, :description, :price, :stock_quantity, presence: true
+
+  # Research AR validations and add a validation that will ensure that all product names are unique and longer than 3 characters.
+  validates :name, uniqueness: true, length: { minimum: 4 }
 end
+
+
+#irb(main):018:0* columns
+#=> ["id: integer", "name: string", "description: string", "price: decimal", "stock_quantity: integer", "category_id: integer", "created_at: datetime", "updated_at: datetime"]
+#irb(main):019:0>
